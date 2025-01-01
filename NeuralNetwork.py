39814e4dce71783ef7419bbed21eb5efd5bbd936
edDenton <edwarddenton06@gmail.com>
2025-01-01 17:31:12 -0500

    Made some updates to AccuracyPlotter so that it prints the results and also includes batch size in the plot Made changes to the Neural Network so that it works generally for the MNIST, MNIST plus letters, and Melanoma datasets Created main functions for each tested data set

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,6 +4,7 @@
 @author: Edward Denton
 """
 import numpy as np
+import string
 from AccuracyPlotter import AccuracyPlotter
 
 
@@ -12,21 +13,21 @@ class NeuralNetwork:
         self.LAYERS = []
         self.EPOCHS = epochs
         self.LEARNING_RATE = learn_rate
+        self.NUMOUTPUTS = layer_sizes[-1]
+
+        keys = string.digits + string.ascii_uppercase + string.ascii_lowercase
+        self.outputToIndex = {key: index for index, key in enumerate(keys)}
         self.plotter = accuracy_plotter
+
         for i in range(len(layer_sizes) - 1):
-            if i == len(layer_sizes) - 2:
-                self.LAYERS.append(Layer(layer_sizes[i], layer_sizes[i + 1], True))
-            else:
-                self.LAYERS.append(Layer(layer_sizes[i], layer_sizes[i + 1], False))
+            is_output_layer = (i == len(layer_sizes) - 2)
+            self.LAYERS.append(Layer(layer_sizes[i], layer_sizes[i + 1], is_output_layer))
 
     def oneHotEncoding(self, training_labels: np.array):
-        oneHotArrays = []
-        for i in range(len(training_labels)):
-            oneHotArray = np.array([0 for _ in range(10)])
-            oneHotArray[training_labels[i]] = 1
-            oneHotArray.reshape(len(oneHotArray), 1)
-            oneHotArrays.append(oneHotArray)
-        return np.transpose(np.array(oneHotArrays))
+        oneHotArray = np.zeros((self.NUMOUTPUTS, len(training_labels)))
+        for i, label in enumerate(training_labels):
+            oneHotArray[self.outputToIndex[str(label)], i] = 1
+        return oneHotArray
 
     def forward_propagation(self, inputs: np.array):
         for layer in self.LAYERS:
@@ -34,32 +35,48 @@ class NeuralNetwork:
         return inputs
 
     def back_propagation(self, outputs: np.array, training_labels: np.array):
-        outputs = 2 * (outputs - self.oneHotEncoding(training_labels))
-        self.LAYERS[-1].costGradientWeights = (1 / len(training_labels)) * outputs.dot(
-            np.transpose(self.LAYERS[-2].layerNodeInfo.activationValues))
-        self.LAYERS[-1].calculateGradients(outputs, training_labels)
+        batch_size = len(training_labels)
+        one_hot_labels = self.oneHotEncoding(training_labels)
+        gradient = outputs - one_hot_labels
+
+        self.LAYERS[-1].calculateGradients(gradient, batch_size)
 
         for i in range(len(self.LAYERS) - 2, -1, -1):
-            outputs = np.transpose(self.LAYERS[i + 1].weights).dot(outputs) * self.LAYERS[i].ReLUDerivative(
-                self.LAYERS[i].layerNodeInfo.preActivationValues)
-            self.LAYERS[i].calculateGradients(outputs, training_labels)
+            gradient = np.dot(self.LAYERS[i + 1].weights.T, gradient) * self.LAYERS[i].ReLUDerivative(
+                self.LAYERS[i].layerNodeInfo.preActivationValues
+            )
+            self.LAYERS[i].calculateGradients(gradient, batch_size)
 
-    def updateWeightsBiases(self, learn_rate: float):
+    def updateWeightsBiases(self):
         for layer in self.LAYERS:
-            layer.updateWeightsBiases(learn_rate)
+            layer.updateWeightsBiases(self.LEARNING_RATE)
 
     def prediction_accuracy(self, outputs: np.array, training_labels: np.array):
-        return np.sum(np.argmax(outputs, 0) == training_labels) / len(training_labels)
+        predictions = np.argmax(outputs, axis=0)
+        return np.sum(predictions == training_labels) / len(training_labels)
 
-    def train(self, training_images: np.array, training_labels: np.array):
+    def train(self, training_images: np.array, training_labels: np.array, batch_size: int):
+        num_samples = len(training_labels)
         for epoch in range(self.EPOCHS):
-            outputs = self.forward_propagation(training_images)
+            indices = np.arange(num_samples)
+            np.random.shuffle(indices)
+            training_images = training_images[:, indices]
+            training_labels = training_labels[indices]
+            training_accuracy = []
 
-            self.plotter.appendData(epoch=epoch,
-                                    accuracy=self.prediction_accuracy(outputs, training_labels))
+            for start in range(0, num_samples, batch_size):
+                end = min(start + batch_size, num_samples)
+                batch_images = training_images[:, start:end]
+                batch_labels = training_labels[start:end]
+                outputs = self.forward_propagation(batch_images)
 
-            self.back_propagation(outputs, training_labels)
-            self.updateWeightsBiases(self.LEARNING_RATE)
+                training_accuracy.append(self.prediction_accuracy(outputs, batch_labels))
+
+                self.back_propagation(outputs, batch_labels)
+                self.updateWeightsBiases()
+
+            self.plotter.appendTrainingData(epoch=epoch,
+                                            accuracy=(sum(training_accuracy) / len(training_accuracy)))
 
     def test(self, test_images: np.array, test_labels: np.array):
         outputs = self.forward_propagation(test_images)
@@ -85,42 +102,37 @@ class Layer:
 
         self.layerNodeInfo = LayerNodeInfo()
 
-        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(numNodesOut, numNodesIn))
+        self.weights = np.random.randn(numNodesOut, numNodesIn) * np.sqrt(2 / numNodesIn)
         self.biases = np.zeros((numNodesOut, 1))
+
         self.costGradientWeights = np.zeros((numNodesOut, numNodesIn))
         self.costGradientBiases = np.zeros((numNodesOut, 1))
 
     def calculateOutputs(self, inputs: np.array):
         self.layerNodeInfo.nodeValues = inputs
-        self.layerNodeInfo.preActivationValues = np.array(np.dot(self.weights, inputs) + self.biases)
+        self.layerNodeInfo.preActivationValues = np.dot(self.weights, inputs) + self.biases
+
         if self.outputLayer:
             self.layerNodeInfo.activationValues = self.softmax(self.layerNodeInfo.preActivationValues)
-            return self.layerNodeInfo.activationValues
         else:
             self.layerNodeInfo.activationValues = self.ReLU(self.layerNodeInfo.preActivationValues)
-            return self.layerNodeInfo.activationValues
 
-    def calculateGradients(self, outputs: np.array, training_labels: np.array):
-        self.costGradientWeights = (1 / len(training_labels)) * outputs.dot(np.transpose(self.layerNodeInfo.nodeValues))
-        self.costGradientBiases = (1 / len(training_labels)) * np.sum(outputs)
-        NONE = None
+        return self.layerNodeInfo.activationValues
+
+    def calculateGradients(self, outputs: np.array, batch_size: int):
+        self.costGradientWeights = (1 / batch_size) * np.dot(outputs, self.layerNodeInfo.nodeValues.T)
+        self.costGradientBiases = (1 / batch_size) * np.sum(outputs, axis=1, keepdims=True)
 
     def updateWeightsBiases(self, learn_rate: float):
-        self.weights = self.weights - learn_rate * self.costGradientWeights
-        self.biases = self.biases - learn_rate * self.costGradientBiases
-        NONE = None
+        self.weights -= learn_rate * self.costGradientWeights
+        self.biases -= learn_rate * self.costGradientBiases
 
     def ReLU(self, inputs: np.array):
         return np.maximum(0, inputs)
 
     def ReLUDerivative(self, inputs: np.array):
-        return inputs > 0
-
-    def sigmoid(self, inputs: np.array):
-        return 1 / (1 + np.exp(-inputs))
-
-    def sigmoidDerivative(self, inputs: np.array):
-        return inputs * (1 - inputs)
+        return (inputs > 0).astype(float)
 
     def softmax(self, inputs: np.array):
-        return np.exp(inputs) / np.sum(np.exp(inputs), axis=0)
+        exp_values = np.exp(inputs - np.max(inputs, axis=0, keepdims=True))  # Stable softmax
+        return exp_values / np.sum(exp_values, axis=0, keepdims=True)
---
+++ melanomaMain.py
@@ -0,0 +1,141 @@
+"""
+Data used for this comes from https://www.kaggle.com/competitions/siim-isic-melanoma-classification/
+
+@author: Edward Denton
+"""
+
+from os import listdir
+import numpy as np
+import pandas as pd
+import random as rand
+import cv2
+import concurrent.futures
+
+from matplotlib import pyplot as plt
+from NeuralNetwork import NeuralNetwork
+from AccuracyPlotter import AccuracyPlotter
+
+FILEPATH = "melanomaData/"
+
+
+def resize_and_save_image(image_path, save_path):
+    image = cv2.imread(image_path)
+    resizedImage = cv2.resize(src=image, dsize=(128, 128))
+    cv2.imwrite(save_path, resizedImage)
+
+
+def resize_images():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+
+        for filename in listdir(FILEPATH + "test"):
+            image_path = FILEPATH + "test/" + filename
+            save_path = FILEPATH + "reTest/re" + filename
+            futures.append(executor.submit(resize_and_save_image, image_path, save_path))
+
+        for filename in listdir(FILEPATH + "train"):
+            image_path = FILEPATH + "train/" + filename
+            save_path = FILEPATH + "reTrain/re" + filename
+            futures.append(executor.submit(resize_and_save_image, image_path, save_path))
+
+        for future in concurrent.futures.as_completed(futures):
+            pass
+
+
+def processData():
+    columnNames = []
+    redPixels = [f"pixel{i} (R)" for i in range(128 * 128)]
+    greenPixels = [f"pixel{i} (G)" for i in range(128 * 128)]
+    bluePixels = [f"pixel{i} (B)" for i in range(128 * 128)]
+    for i in range(128 * 128):
+        columnNames.append(redPixels[i])
+        columnNames.append(greenPixels[i])
+        columnNames.append(bluePixels[i])
+    columnNames.insert(0, "label")
+
+    dfTrainCSV = pd.read_csv(FILEPATH + "train.csv")
+    dataRows = []
+    for i in range(len(dfTrainCSV)):
+        image = cv2.imread(FILEPATH + "reTrain/re" + dfTrainCSV.iloc[i, 0] + ".jpg")
+        image = image.flatten()
+        label = 0 if dfTrainCSV.iloc[i, 6] == "benign" else 1
+        newRow = [label] + image.tolist()
+        dataRows.append(newRow)
+
+    dataDF = pd.DataFrame(dataRows, columns=columnNames)
+    print(dataDF)
+    dataDF.to_csv(FILEPATH + "completeMelanomaData.csv", index=False)
+    print("Done")
+
+
+def shrinkData():
+    data = pd.read_csv(FILEPATH + "completeMelanomaData.csv")
+    data.sort_values(by="label", ascending=False, inplace=True)
+
+    malignant_rows = data[data["label"] == 1].to_numpy()
+    benign_rows = data[data["label"] == 0].to_numpy()
+
+    print(malignant_rows.shape)
+    print(benign_rows.shape)
+    np.random.shuffle(malignant_rows)
+    np.random.shuffle(benign_rows)
+    benign_rows = benign_rows[:3000]
+
+    combined_rows = np.vstack((malignant_rows, benign_rows))
+    combined_df = pd.DataFrame(combined_rows, columns=data.columns)
+    combined_df.to_csv(FILEPATH + "melanomaData.csv", index=False)
+
+
+def getData():
+    malignant_train = 400
+    benign_train = 400
+
+    data = pd.read_csv(FILEPATH + "melanomaData.csv")
+    data.sort_values(by="label", ascending=False, inplace=True)
+
+    malignant_rows = data[data["label"] == 1].to_numpy()
+    benign_rows = data[data["label"] == 0].to_numpy()
+
+    np.random.shuffle(malignant_rows)
+    np.random.shuffle(benign_rows)
+
+    training_data = np.vstack((malignant_rows[:malignant_train], benign_rows[:benign_train]))
+    testing_data = np.vstack((malignant_rows[malignant_train:], benign_rows[benign_train:]))
+
+    np.random.shuffle(training_data)
+    np.random.shuffle(testing_data)
+
+    testing_data = np.transpose(testing_data)
+    testing_labels = testing_data[0]
+    testing_images = np.array(testing_data[1:] / 255.0, dtype=np.float64)
+
+    training_data = np.transpose(training_data)
+    training_labels = training_data[0]
+    training_images = np.array(training_data[1:] / 255.0, dtype=np.float64)
+
+    return training_images, training_labels, testing_images, testing_labels
+
+
+def main():
+    LR = 0.0005
+    EPOCHS = 500
+    LAYERS = [49152, 1024, 512, 2]
+    BATCH_SIZE = 512
+
+    # shrinkData()
+    # resize_images()
+    # processData()
+    dataPlotter = AccuracyPlotter(learn_rate=LR, epochs=EPOCHS, layers=LAYERS, batch_size=BATCH_SIZE)
+    neural_network = NeuralNetwork(layer_sizes=LAYERS, learn_rate=LR, epochs=EPOCHS, accuracy_plotter=dataPlotter)
+    training_images, training_labels, testing_images, testing_labels = getData()
+    print("Data has been gathered")
+    neural_network.train(training_images, training_labels, BATCH_SIZE)
+    print("Finished Training")
+    neural_network.test(testing_images, testing_labels)
+    print("Finished Testing")
+    dataPlotter.showPlot()
+    # seePerformance(neural_network, training_images, training_labels)
+
+
+if __name__ == '__main__':
+    main()
